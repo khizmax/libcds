74e081695a9fe0f6623874d3feb810c6526239cf
khizmax <libcds.dev@gmail.com>
2014-10-20 10:35:26 +0400

    Fix CDS_NOEXCEPT

--- cds/lock/spinlock.h
+++ cds/lock/spinlock.h
@@ -126,7 +126,7 @@ namespace cds {
                 Returns \p true if locking is succeeded
                 otherwise (if the spin is already locked) returns \p false
             */
-            bool try_lock( unsigned int nTryCount ) CDS_NOEXCEPT( noexcept( backoff_strategy()() ) )
+            bool try_lock( unsigned int nTryCount ) CDS_NOEXCEPT_( noexcept( backoff_strategy()() ) )
             {
                 backoff_strategy backoff;
                 while ( nTryCount-- ) {
@@ -138,7 +138,7 @@ namespace cds {
             }
 
             /// Lock the spin-lock. Waits infinitely while spin-lock is locked. Debug version: deadlock may be detected
-            void lock() CDS_NOEXCEPT(noexcept( backoff_strategy()() ))
+            void lock() CDS_NOEXCEPT_(noexcept( backoff_strategy()() ))
             {
                 backoff_strategy backoff;
 
