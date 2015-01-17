3c4ce94d5f7f794033387ac38cb90b0aa4a97791
Marsel Galimullin <mfgalimullin@yandex.ru>
2015-01-17 23:08:27 +0500

    flat combining test change

--- tests/test-hdr/queue/hdr_queue_register.cpp
+++ tests/test-hdr/queue/hdr_queue_register.cpp
@@ -1,4 +1,13 @@
-//$$CDS-header$$
+/*
+    This file is a part of libcds - Concurrent Data Structures library
+    Version: 2.0.0
+
+    (C) Copyright Maxim Khizhinsky (libcds.dev@gmail.com) 2006-2014
+    Distributed under the BSD license (see accompanying file license.txt)
+
+    Source code repo: http://github.com/khizmax/libcds/
+    Download: http://sourceforge.net/projects/libcds/files/
+*/
 
 #include "hdr_intrusive_msqueue.h"
 #include "hdr_intrusive_segmented_queue.h"
@@ -6,8 +15,8 @@
 #include "hdr_fcqueue.h"
 #include "hdr_segmented_queue.h"
 
-CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrTestQueue,               s_HdrTestQueue );
-CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrFCQueue,           s_Queue_TestHeader ); //TODO must be removed after refactoring
-CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrSegmentedQueue,          s_HdrSegmentedQueue );
-CPPUNIT_TEST_SUITE_REGISTRATION_( queue::IntrusiveQueueHeaderTest,   s_IntrusiveQueueHeaderTest );
-CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrIntrusiveSegmentedQueue, s_HdrIntrusiveSegmentedQueue );
+//CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrTestQueue,               s_HdrTestQueue );
+CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrFCQueue,                 s_Queue_TestHeader ); //TODO must be removed after refactoring
+//CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrSegmentedQueue,          s_HdrSegmentedQueue );
+//CPPUNIT_TEST_SUITE_REGISTRATION_( queue::IntrusiveQueueHeaderTest,   s_IntrusiveQueueHeaderTest );
+//CPPUNIT_TEST_SUITE_REGISTRATION_( queue::HdrIntrusiveSegmentedQueue, s_HdrIntrusiveSegmentedQueue );
