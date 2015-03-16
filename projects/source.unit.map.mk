ded4ac486f04a6ffdcd776adefd4db3e8efee38e
khizmax <libcds.dev@gmail.com>
2015-03-16 22:36:50 +0300

    Splitted map_insdel_func test for reducing compiler memory requirements

--- projects/source.unit.map.mk
+++ projects/source.unit.map.mk
@@ -3,6 +3,13 @@ CDSUNIT_MAP_SOURCES := \
     tests/unit/map2/map_find_int.cpp \
     tests/unit/map2/map_find_string.cpp \
     tests/unit/map2/map_insdel_func.cpp \
+    tests/unit/map2/map_insdel_func2.cpp \
+    tests/unit/map2/map_insdel_func3.cpp \
+    tests/unit/map2/map_insdel_func4.cpp \
+    tests/unit/map2/map_insdel_func5.cpp \
+    tests/unit/map2/map_insdel_func6.cpp \
+    tests/unit/map2/map_insdel_func7.cpp \
+    tests/unit/map2/map_insdel_func8.cpp \
     tests/unit/map2/map_insdel_int.cpp \
     tests/unit/map2/map_insdel_item_int.cpp \
     tests/unit/map2/map_insdel_string.cpp \
