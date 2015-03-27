9a35c624a29ac38eff954dc8d885566df58a3305
khizmax <khizmax@gmail.com>
2015-03-27 15:23:51 +0300

    Splitted up set_insdel_string test

---
+++ tests/unit/set2/set_insdel_string_michael.cpp
@@ -0,0 +1,9 @@
+//$$CDS-header$$
+
+#include "set2/set_insdel_string.h"
+
+namespace set2 {
+    CPPUNIT_TEST_SUITE_PART( Set_InsDel_string, run_MichaelSet )
+        CDSUNIT_TEST_MichaelSet
+    CPPUNIT_TEST_SUITE_END_PART()
+} // namespace set2
---
+++ tests/unit/set2/set_insdel_string_striped.cpp
@@ -0,0 +1,9 @@
+//$$CDS-header$$
+
+#include "set2/set_insdel_string.h"
+
+namespace set2 {
+    CPPUNIT_TEST_SUITE_PART( Set_InsDel_string, run_StripedSet )
+        CDSUNIT_TEST_StripedSet
+    CPPUNIT_TEST_SUITE_END_PART()
+} // namespace set2
