0be641638ba7a041e2f6c5d1470b1d5d4ae317f7
khizmax <khizmax@gmail.com>
2016-03-14 15:40:17 +0300

    Docfix

--- cds/container/impl/feldman_hashmap.h
+++ cds/container/impl/feldman_hashmap.h
@@ -709,7 +709,7 @@ namespace cds { namespace container {
 
         /// Collects tree level statistics into \p stat
         /**
-            The function traverses the set and collects staistics for each level of the tree
+            The function traverses the set and collects statistics for each level of the tree
             into \p feldman_hashset::level_statistics struct. The element of \p stat[i]
             represents statistics for level \p i, level 0 is head array.
             The function is thread-safe and may be called in multi-threaded environment.
