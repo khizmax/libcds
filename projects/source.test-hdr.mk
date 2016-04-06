3584c62b9f894a8e63d39b9ea90b012fa85bba78
khizmax <libcds.dev@gmail.com>
2016-04-07 00:41:36 +0300

    Removed old map unit tests

--- projects/source.test-hdr.mk
+++ projects/source.test-hdr.mk
@@ -1,69 +1,3 @@
-CDS_TESTHDR_MAP := \
-    tests/test-hdr/map/hdr_michael_map_hp.cpp \
-    tests/test-hdr/map/hdr_michael_map_dhp.cpp \
-    tests/test-hdr/map/hdr_michael_map_rcu_gpi.cpp \
-    tests/test-hdr/map/hdr_michael_map_rcu_gpb.cpp \
-    tests/test-hdr/map/hdr_michael_map_rcu_gpt.cpp \
-    tests/test-hdr/map/hdr_michael_map_rcu_shb.cpp \
-    tests/test-hdr/map/hdr_michael_map_rcu_sht.cpp \
-    tests/test-hdr/map/hdr_michael_map_nogc.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_hp.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_dhp.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_rcu_gpi.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_rcu_gpb.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_rcu_gpt.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_rcu_shb.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_rcu_sht.cpp \
-    tests/test-hdr/map/hdr_michael_map_lazy_nogc.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_hp.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_dhp.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_rcu_gpb.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_rcu_gpi.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_rcu_gpt.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_rcu_shb.cpp \
-    tests/test-hdr/map/hdr_feldman_hashmap_rcu_sht.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_hashmap_std.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_boost_list.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_list.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_map.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_boost_map.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_boost_flat_map.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_boost_unordered_map.cpp \
-    tests/test-hdr/map/hdr_refinable_hashmap_slist.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_hp.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_dhp.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_rcu_gpi.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_rcu_gpb.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_rcu_gpt.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_rcu_shb.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_rcu_sht.cpp \
-    tests/test-hdr/map/hdr_skiplist_map_nogc.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_hp.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_dhp.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_nogc.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_rcu_gpi.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_rcu_gpb.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_rcu_gpt.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_rcu_shb.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_rcu_sht.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_hp.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_dhp.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_nogc.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_rcu_gpi.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_rcu_gpb.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_rcu_gpt.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_rcu_sht.cpp \
-    tests/test-hdr/map/hdr_splitlist_map_lazy_rcu_shb.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_hashmap_std.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_boost_list.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_list.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_map.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_boost_map.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_boost_flat_map.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_boost_unordered_map.cpp \
-    tests/test-hdr/map/hdr_striped_hashmap_slist.cpp \
-    tests/test-hdr/map/hdr_striped_map_reg.cpp
-
 CDS_TESTHDR_TREE := \
     tests/test-hdr/tree/hdr_tree_reg.cpp \
     tests/test-hdr/tree/hdr_intrusive_ellen_bintree_hp.cpp \
@@ -112,7 +46,6 @@ CDS_TESTHDR_MISC := \
     tests/test-hdr/misc/thread_init_fini.cpp
 
 CDS_TESTHDR_SOURCES := \
-    $(CDS_TESTHDR_MAP) \
     $(CDS_TESTHDR_TREE) \
     $(CDS_TESTHDR_MISC)
     
--- projects/source.test-hdr.offsetof.mk
+++ projects/source.test-hdr.offsetof.mk
@@ -1,6 +1,3 @@
-CDS_TESTHDR_OFFSETOF_MAP := \
-    tests/test-hdr/map/hdr_cuckoo_map.cpp 
-
 CDS_TESTHDR_OFFSETOF_TREE := \
     tests/test-hdr/tree/hdr_intrusive_ellen_bintree_hp_member.cpp \
     tests/test-hdr/tree/hdr_intrusive_ellen_bintree_dhp_member.cpp \
@@ -11,5 +8,4 @@ CDS_TESTHDR_OFFSETOF_TREE := \
     tests/test-hdr/tree/hdr_intrusive_ellen_bintree_rcu_sht_member.cpp
 
 CDS_TESTHDR_OFFSETOF_SOURCES := \
-	$(CDS_TESTHDR_OFFSETOF_MAP) \
 	$(CDS_TESTHDR_OFFSETOF_TREE)
