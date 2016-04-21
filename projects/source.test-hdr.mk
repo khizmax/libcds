ff3b19a7120f7a8e60731ad276615d1e9abac595
khizmax <libcds.dev@gmail.com>
2016-04-21 23:38:24 +0300

    Removed old tree unit test

--- projects/source.test-hdr.mk
+++ projects/source.test-hdr.mk
@@ -1,38 +1,3 @@
-CDS_TESTHDR_TREE := \
-    tests/test-hdr/tree/hdr_tree_reg.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_hp.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_dhp.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_rcu_gpb.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_rcu_gpi.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_rcu_gpt.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_rcu_shb.cpp \
-    tests/test-hdr/tree/hdr_intrusive_ellen_bintree_rcu_sht.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_hp.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_dhp.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_rcu_gpb.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_rcu_gpi.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_rcu_gpt.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_rcu_shb.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_map_rcu_sht.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_hp.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_dhp.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_rcu_gpb.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_rcu_gpi.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_rcu_gpt.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_rcu_shb.cpp \
-    tests/test-hdr/tree/hdr_ellenbintree_set_rcu_sht.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_gpb.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_gpb_pool_monitor.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_gpi.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_gpi_pool_monitor.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_gpt.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_gpt_pool_monitor.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_shb.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_shb_pool_monitor.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_sht.cpp \
-    tests/test-hdr/tree/hdr_bronson_avltree_map_rcu_sht_pool_monitor.cpp \
-    
-
 CDS_TESTHDR_MISC := \
     tests/test-hdr/misc/cxx11_atomic_class.cpp \
     tests/test-hdr/misc/cxx11_atomic_func.cpp \
@@ -46,6 +11,5 @@ CDS_TESTHDR_MISC := \
     tests/test-hdr/misc/thread_init_fini.cpp
 
 CDS_TESTHDR_SOURCES := \
-    $(CDS_TESTHDR_TREE) \
     $(CDS_TESTHDR_MISC)
     
