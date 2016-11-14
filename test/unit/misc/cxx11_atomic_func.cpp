a8f8957d8fd4d314e30f3bcb08d32a60074dd72f
khizmax <libcds.dev@gmail.com>
2016-11-14 21:39:43 +0300

    Fixed clang 3.5,3.6 incompatibilities

--- test/unit/misc/cxx11_atomic_func.cpp
+++ test/unit/misc/cxx11_atomic_func.cpp
@@ -706,8 +706,8 @@ namespace misc {
         test_atomic_integral_volatile<unsigned long long>();
     }
 
-#if !( CDS_COMPILER == CDS_COMPILER_CLANG && defined(_LIBCPP_VERSION) && CDS_COMPILER_VERSION < 40000 )
-    //clang error (libc++) with atomic<void> fetch_add/fetch_sub
+#if !( CDS_COMPILER == CDS_COMPILER_CLANG && CDS_COMPILER_VERSION < 40000 )
+    //clang error with atomic<void*> fetch_add/fetch_sub
     TEST_F( cxx11_atomic_func, atomic_pointer_void )
     {
         do_test_atomic_pointer_void<false>();
