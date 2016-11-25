1a676ff9146742842d24e7cd09faac6dd3980807
khizmax <khizmax@gmail.com>
2016-11-25 15:14:14 +0300

    Minor changes in VC auto-build scripts

--- projects/Win/build-msbuild.cmd
+++ projects/Win/build-msbuild.cmd
@@ -10,5 +10,5 @@ set MSBUILD=msbuild
 :make
 for %%A in (x64 Win32) do (
   %MSBUILD% %vcXX%\cds.sln /t:Clean /p:Configuration=%cfg% /p:Platform=%%A %3 %4 %5 %6 %7 %8 %9
-  %MSBUILD% %vcXX%\cds.sln /t:Build /p:Configuration=%cfg% /p:Platform=%%A /fl /flp:LogFile=%vcXX%_%%A.log;Encoding=UTF-8;Verbosity=normal /v:normal /maxcpucount %3 %4 %5 %6 %7 %8 %9
+  %MSBUILD% %vcXX%\cds.sln /t:Build /p:Configuration=%cfg% /p:Platform=%%A /fl /flp:LogFile=%vcXX%_%%A.log;Encoding=UTF-8;Verbosity=normal /v:normal %3 %4 %5 %6 %7 %8 %9
 )
--- projects/Win/build-vc14.cmd
+++ projects/Win/build-vc14.cmd
@@ -1,2 +1,2 @@
 call "%VS140COMNTOOLS%"\vsvars32.bat
-call build-msbuild.cmd vc14 Release /tv:14.0 /m
+call build-msbuild.cmd vc14 Release /tv:14.0 /m:2
