3b33170ca71c71bf1739c4a64cb505664a14a9fe
XRater <kair1998@gmail.com>
2017-06-12 20:10:16 +0300

    Add files via upload

---
+++ nc_feldman_hashset_dhp.h
@@ -0,0 +1,7 @@
+#ifndef CDSLIB_CONTAINER_NC_FELDMAN_HASHSET_DHP_H
+#define CDSLIB_CONTAINER_NC_FELDMAN_HASHSET_DHP_H
+
+#include <cds/container/impl/nc_feldman_hashset.h>
+#include <cds/gc/dhp.h>
+
+#endif // #ifndef CDSLIB_CONTAINER_NC_FELDMAN_HASHSET_DHP_H
---
+++ nc_feldman_hashset_hp.h
@@ -0,0 +1,7 @@
+#ifndef CDSLIB_CONTAINER_NC_FELDMAN_HASHSET_HP_H
+#define CDSLIB_CONTAINER_NC_FELDMAN_HASHSET_HP_H
+
+#include <cds/container/impl/nc_feldman_hashset.h>
+#include <cds/gc/hp.h>
+
+#endif // #ifndef CDSLIB_CONTAINER_NC_FELDMAN_HASHSET_HP_H
