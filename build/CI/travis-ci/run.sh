4dad8e7e752debfe01c50f17627d0f9697131f6c
khizmax <khizmax@gmail.com>
2018-03-18 20:04:19 +0300

    Fixed conan on Darwin

--- build/CI/travis-ci/run.sh
+++ build/CI/travis-ci/run.sh
@@ -3,17 +3,20 @@
 set -e
 set -x
 
+CONAN_INSTALL_FLAGS="-s compiler.libcxx=libstdc++11"
+
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
     pyenv activate conan
+    CONAN_INSTALL_FLAGS=""
 fi
 
 #export CXX=$CXX_COMPILER
 #export CC=$C_COMPILER
 mkdir build-test && cd build-test
-conan install --build=missing -s compiler.libcxx=libstdc++11 -s build_type=$BUILD_TYPE ..
+conan install --build=missing $CONAN_INSTALL_FLAGS -s build_type=$BUILD_TYPE ..
 cmake -DCMAKE_PREFIX_PATH="$TRAVIS_BUILD_DIR/build-test/deps" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXE_LINKER_FLAGS=$LINKER_FLAGS -DWITH_TESTS=ON ..
 cmake --build . -- -j2 $TARGET
 
