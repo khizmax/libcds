f144df84c2eb70737ee94e32c3c4f51980a1d086
khizmax <khizmax@gmail.com>
2018-03-29 21:14:00 +0300

    rewritten to reduce memory consumption

--- test/unit/misc/bit_reversal.cpp
+++ test/unit/misc/bit_reversal.cpp
@@ -12,28 +12,16 @@ namespace {
     class bit_reversal: public ::testing::Test
     {
         typedef UInt uint_type;
-        static std::vector<uint_type> arr_;
-        static size_t const c_size = 100'000'000;
+        static size_t const c_size = 50'000'000;
 
     public:
-        static void SetUpTestCase()
-        {
-            arr_.resize( c_size );
-            for ( size_t i = 0; i < c_size; ++i )
-                arr_[i] = static_cast<uint_type>((i << 32) + (~i));
-        }
-
-        static void TearDownTestCase()
-        {
-            arr_.resize( 0 );
-        }
-
         template <typename Algo>
         void test()
         {
             Algo f;
-            for ( auto i : arr_ ) {
-                EXPECT_EQ( i, f( f( i )));
+            for ( uint_type i = 0; i < c_size; ++i ) {
+                EXPECT_EQ( i, f( f( i ) ));
+                EXPECT_EQ( ~i, f( f( ~i ) ));
             }
         }
 
@@ -41,13 +29,12 @@ namespace {
         void test_eq()
         {
             Algo f;
-            for ( auto i : arr_ ) {
-                EXPECT_EQ( cds::algo::bit_reversal::swar()( i ), f( i )) << "i=" << i;
+            for ( uint_type i = 0; i < c_size; ++i ) {
+                EXPECT_EQ( cds::algo::bit_reversal::swar()( i ), f( i ) ) << "i=" << i;
+                EXPECT_EQ( cds::algo::bit_reversal::swar()( ~i ), f( ~i ) ) << "~i=" << ~i;
             }
         }
     };
-
-    template <typename UInt> std::vector<UInt> bit_reversal<UInt>::arr_;
 
     typedef bit_reversal<uint32_t> bit_reversal32;
     typedef bit_reversal<uint64_t> bit_reversal64;
