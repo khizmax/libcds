2ccc25bbc2c29a713949e32d15b8083d06253500
khizmax <khizmax@gmail.com>
2018-11-10 11:21:20 +0300

    Change version to 2.3.3

--- cds/version.h
+++ cds/version.h
@@ -8,14 +8,14 @@
 
 // cds library version
 
-#define CDS_VERSION        0x020400
+#define CDS_VERSION        0x020303
 
 #define CDS_VERSION_MAJOR  ((CDS_VERSION & 0xFF0000)>> 16)
 #define CDS_VERSION_MINOR  ((CDS_VERSION & 0x00FF00) >> 8)
 #define CDS_VERSION_PATCH  (CDS_VERSION & 0x0000FF)
 
 // CDS_VERSION == CDS_VERSION_MAJOR '.' CDS_VERSION_MINOR '.' CDS_VERSION_PATCH
-#define CDS_VERSION_STRING        "2.4.0"
+#define CDS_VERSION_STRING        "2.3.3"
 
 #endif  // #ifndef CDSLIB_VERSION_H
 
