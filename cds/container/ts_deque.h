9d2f2afd5f97aefbbebeee89e210ec9a8ea80032
Alexey Chirukhin <pr3sto1377@gmail.com>
2018-12-04 19:23:12 +0300

    Init ts_deque

---
+++ cds/container/ts_deque.h
@@ -0,0 +1,25 @@
+template<typename T, typename Timestamp>
+class TSDeque {
+  private:
+
+  public:
+    TSDeque (uint64_t num_threads, uint64_t delay)
+    {
+    }
+
+    bool insert_left(T element)
+    {
+    }
+
+    bool insert_right(T element)
+    {
+    }
+
+    bool remove_left(T *element)
+    {
+    }
+
+    bool remove_right(T *element)
+    {
+    }
+};
