ab723b5c2fa4ca1680de931f40038a6a0f790397
vikkiorrikki <vikulka58@mail.ru>
2018-12-15 23:24:16 +0300

    timestamp hardware interval

--- cds/container/ts_timestamp.h
+++ cds/container/ts_timestamp.h
@@ -6,6 +6,12 @@ inline uint64_t get_hwptime(void)
     return (rdx << 32) + rax;
 }
 
+inline uint64_t get_hwtime(void) {
+  unsigned int hi, lo;
+  __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+  return ((uint64_t) lo) | (((uint64_t) hi) << 32);
+}
+
 class HardwareTimestamp {
   public:
     inline void initialize(uint64_t delay, uint64_t num_threads) {
@@ -41,5 +47,56 @@ class HardwareTimestamp {
 
     inline bool is_later(uint64_t *timestamp1, uint64_t *timestamp2) {
       return timestamp2[0] < timestamp1[0];
+    }
+};
+
+class HardwareIntervalTimestamp {
+  private:
+    uint64_t delay_;
+
+  public:
+    inline void initialize(uint64_t delay, uint64_t num_threads) {
+      delay_ = delay;
+    }
+
+    inline void init_sentinel(uint64_t *result) {
+      result[0] = 0;
+      result[1] = 0;
+    }
+
+    inline void init_sentinel_atomic(std::atomic<uint64_t> *result) {
+      result[0].store(0);
+      result[1].store(0);
+    }
+
+    inline void init_top_atomic(std::atomic<uint64_t> *result) {
+      result[0].store(UINT64_MAX);
+      result[1].store(UINT64_MAX);
+    }
+
+    inline void init_top(uint64_t *result) {
+      result[0] = UINT64_MAX;
+      result[1] = UINT64_MAX;
+    }
+
+    inline void load_timestamp(uint64_t *result, std::atomic<uint64_t> *source) {
+      result[0] = source[0].load();
+      result[1] = source[1].load();
+    }
+
+    inline void set_timestamp(std::atomic<uint64_t> *result) {
+      result[0].store(get_hwptime());
+      uint64_t wait = get_hwtime() + delay_;
+      while (get_hwtime() < wait) {}
+      result[1].store(get_hwptime());
+    }
+
+    inline void read_time(uint64_t *result) {
+      result[0] = get_hwptime();
+      result[1] = result[0];
+    }
+
+    inline bool is_later(uint64_t *timestamp1, uint64_t *timestamp2) {
+      return timestamp2[1] < timestamp1[0];
     }
 };
