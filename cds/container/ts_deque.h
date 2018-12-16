b2a8b91b9bfda01834bef910efa6ba5de71107c6
Alexey Chirukhin <pr3sto1377@gmail.com>
2018-12-16 20:06:11 +0300

    implement ts_deque

--- cds/container/ts_deque.h
+++ cds/container/ts_deque.h
@@ -1,25 +1,72 @@
+#include <atomic>
+#include "ts_deque_buffer.h"
+
 template<typename T, typename Timestamp>
 class TSDeque {
   private:
 
+    TSDequeBuffer<T, Timestamp> *buffer_;
+    Timestamp *timestamping_;
+
   public:
-    TSDeque (uint64_t num_threads, uint64_t delay)
+    TSDeque (uint64_t num_threads)
     {
+
+      timestamping_ = new Timestamp();
+
+      buffer_ = new TSDequeBuffer<T, Timestamp>();
+      buffer_->initialize(num_threads, timestamping_);
     }
 
     bool insert_left(T element)
     {
+      std::atomic<uint64_t> *item = buffer_->insert_left(element);
+      // In the set_timestamp operation first a new timestamp is acquired
+      // and then assigned to the item. The operation may not be executed
+      // atomically.
+      timestamping_->set_timestamp(item);
+      return true;
     }
 
     bool insert_right(T element)
     {
+      std::atomic<uint64_t> *item = buffer_->insert_right(element);
+      // In the set_timestamp operation first a new timestamp is acquired
+      // and then assigned to the item. The operation may not be executed
+      // atomically.
+      timestamping_->set_timestamp(item);
+      return true;
     }
 
     bool remove_left(T *element)
     {
+      // Read the invocation time of this operation, needed for the
+      // elimination optimization.
+      uint64_t invocation_time[2];
+      timestamping_->read_time(invocation_time);
+      while (buffer_->try_remove_left(element, invocation_time)) {
+
+        if (*element != (T)NULL) {
+          return true;
+        }
+      }
+      // The deque was empty, return false.
+      return false;
     }
 
     bool remove_right(T *element)
     {
+      // Read the invocation time of this operation, needed for the
+      // elimination optimization.
+      uint64_t invocation_time[2];
+      timestamping_->read_time(invocation_time);
+      while (buffer_->try_remove_right(element, invocation_time)) {
+
+        if (*element != (T)NULL) {
+          return true;
+        }
+      }
+      // The deque was empty, return false.
+      return false;
     }
 };
