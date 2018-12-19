09052d70d34278799fabe3495bbd4db9978de038
vikkiorrikki <vikulka58@mail.ru>
2018-12-19 20:17:55 +0300

    timestamp atomic counter

--- cds/container/ts_timestamp.h
+++ cds/container/ts_timestamp.h
@@ -1,3 +1,5 @@
+#include <atomic>
+
 inline uint64_t get_hwptime(void)
 {
     uint64_t aux;
@@ -98,5 +100,52 @@ class HardwareIntervalTimestamp {
 
     inline bool is_later(uint64_t *timestamp1, uint64_t *timestamp2) {
       return timestamp2[1] < timestamp1[0];
+    }
+};
+
+class AtomicCounterTimestamp {
+  private:
+    std::atomic<uint64_t> *clock_;
+
+  public:
+    inline void initialize(uint64_t delay, uint64_t num_threads) {
+      clock_ = new std::atomic<uint64_t>();
+      clock_->store(1);
+    }
+
+    inline void init_sentinel(uint64_t *result) {
+      result[0] = 0;
+    }
+
+    inline void init_sentinel_atomic(std::atomic<uint64_t> *result) {
+      result[0].store(0);
+    }
+
+    inline void init_top_atomic(std::atomic<uint64_t> *result) {
+      result[0].store(UINT64_MAX);
+    }
+
+    inline void init_top(uint64_t *result) {
+      result[0] = UINT64_MAX;
+    }
+
+    inline void load_timestamp(uint64_t *result, std::atomic<uint64_t> *source) {
+      result[0] = source[0].load();
+    }
+
+    inline void set_timestamp(std::atomic<uint64_t> *result) {
+      result[0].store(clock_->fetch_add(1));
+    }
+
+    inline void set_timestamp_local(uint64_t *result) {
+      result[0] = clock_->fetch_add(1);
+    }
+
+    inline void read_time(uint64_t *result) {
+      result[0] = clock_->load();
+    }
+
+	inline bool is_later(uint64_t *timestamp1, uint64_t *timestamp2) {
+      return timestamp2[0] < timestamp1[0];
     }
 };
