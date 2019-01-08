6e176cadc188245e129ccc1d596151325941cb8b
AndreyFdrv <andrei_hedorov@mail.ru>
2019-01-09 00:29:07 +0300

    stress/set/stress-set-insdel-func

---
+++ test/stress/set/insdel_func/set_insdel_func_hopscotch.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2006-2018 Maxim Khizhinsky
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "set_insdel_func.h"
+#include "set_type_hopscotch.h"
+
+namespace set {
+
+	CDSSTRESS_HopscotchHashset( Set_InsDel_func, run_test, size_t, value )
+
+} // namespace set
