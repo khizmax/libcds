0e7d7b662ff8576be3cbfd19b03b7191a6154159
AndreyFdrv <andrei_hedorov@mail.ru>
2019-01-08 15:49:06 +0300

    stress/map/stress-map-delodd

---
+++ test/stress/map/delodd/map_delodd_hopscotch.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2006-2018 Maxim Khizhinsky
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "map_delodd.h"
+#include "map_type_hopscotch.h"
+
+namespace map {
+
+	CDSSTRESS_HopscotchHashmap( Map_DelOdd, run_test, key_thread, size_t )
+
+} // namespace map
