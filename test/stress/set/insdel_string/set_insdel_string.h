191a1d03ff40731958d7c5e243eac3d014ee7146
AndreyFdrv <andrei_hedorov@mail.ru>
2019-01-12 02:42:37 +0300

    undo changes in stress/set/stress-set-insdel-string

--- test/stress/set/insdel_string/set_insdel_string.h
+++ test/stress/set/insdel_string/set_insdel_string.h
@@ -75,8 +75,6 @@ namespace set {
 
                 Set_InsDel_string& fixture = pool().template fixture<Set_InsDel_string>();
                 size_t nArrSize = m_arrString.size();
-                if (nArrSize == 0)
-                    return;
                 size_t const nSetSize = fixture.s_nSetSize;
                 size_t const nPassCount = fixture.s_nThreadPassCount;
 
@@ -135,8 +133,6 @@ namespace set {
 
                 Set_InsDel_string& fixture = pool().template fixture<Set_InsDel_string>();
                 size_t nArrSize = m_arrString.size();
-                if (nArrSize == 0)
-                    return;
                 size_t const nSetSize = fixture.s_nSetSize;
                 size_t const nPassCount = fixture.s_nThreadPassCount;
 
@@ -458,7 +454,7 @@ namespace set {
         template <class Set>
         void run_test()
         {
-            ASSERT_TRUE( m_arrString.size() >= 0 );
+            ASSERT_TRUE( m_arrString.size() > 0 );
 
             Set s( *this );
             do_test( s );
