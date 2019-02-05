10f07cc40047b76a3d0fd72168e89bad47b62653
Kompanishchenko Nikita <nik-kompanishhenko@yandex.ru>
2019-02-05 20:35:03 +0300

    Update intrusive_push_pop.cpp

--- test/stress/queue/intrusive_push_pop.cpp
+++ test/stress/queue/intrusive_push_pop.cpp
@@ -2,6 +2,7 @@
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include "intrusive_queue_type.h"
 #include <vector>
 #include <algorithm>
