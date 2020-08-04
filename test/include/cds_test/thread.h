b3cdf63c6485adb4ed118b0cf02f543ad7d4bbe9
krock21ru <krock21rus@gmail.com>
2020-08-04 11:48:03 +0300

    fix HeapTLSManager

--- test/include/cds_test/thread.h
+++ test/include/cds_test/thread.h
@@ -39,12 +39,12 @@ namespace cds_test {
         virtual void SetUp()
         {
             cds::threading::Manager::attachThread();
-            cds::gc::hp::custom_smr<cds::gc::hp::details::HeapTLSManager>::attach_thread();
+            cds::gc::hp::smr::attach_thread();
         }
 
         virtual void TearDown()
         {
-            cds::gc::hp::custom_smr<cds::gc::hp::details::HeapTLSManager>::detach_thread();
+            cds::gc::hp::smr::detach_thread();
             cds::threading::Manager::detachThread();
         }
 
