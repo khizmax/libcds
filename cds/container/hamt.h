04c6226e1c4d749e527f258576565ad9eaa40096
damirJann <pilacis@list.ru>
2022-04-15 00:41:51 +0300

    Init commit

---
+++ cds/container/hamt.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2006-2018 Maxim Khizhinsky
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CDSLIB_CONTAINER_HAMT_H
+#define CDSLIB_CONTAINER_HAMT_H
+
+namespace cds { namespace container {
+    namespace hamt {
+
+
+    } // namespace hamt
+}} // namespace cds::container
+
+#endif // #ifndef CDSLIB_CONTAINER_HAMT_H
